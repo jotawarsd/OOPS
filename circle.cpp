fbdc66e47f6250563d7ada578645ff2c79d1371a
jotawarsd <64022045+jotawarsd@users.noreply.github.com>
2021-08-24 15:16:23 +0530

    lab1

---
+++ circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,area;
+    cout<<"enter radius of circle"<<endl;
+    cin>>a;
+    area = 3.14159*a*a;
+    cout<<"area of circle is "<<area<<endl;
+    return 0;
+}
